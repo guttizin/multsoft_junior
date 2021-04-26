3b97d36458a6eb48cba68ca3516a1edc9f6ebaad
carlos.dias <carlos.dias@gpxsolucao.com.br>
2021-04-26 01:43:34 -0300

    Inicial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'multsoft-angular';
+
+  menuOpened = false;
+
+  listTitles = [
+    {
+      trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+      cover: 'assets/images/guardios-da-galaxia.jpg',
+      name: 'Guardiões da Galaxia',
+      relevance: 98,
+      age: 0,
+      parts: 2,
+      categories: ['Épico', 'Filme de Fantasia', 'Viagem no espaço']
+    },
+    {
+      trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+      cover: 'assets/images/guardios-da-galaxia.jpg',
+      name: 'Guardiões da Galaxia',
+      relevance: 55,
+      age: 12,
+      parts: 1,
+      categories: ['Épico', 'Filme de Fantasia', 'Viagem no espaço']
+    },
+    {
+      trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+      cover: 'assets/images/guardios-da-galaxia.jpg',
+      name: 'Guardiões da Galaxia',
+      relevance: 100,
+      age: 16,
+      parts: 5,
+      categories: ['Épico', 'Filme de Fantasia', 'Viagem no espaço']
+    },
+    {
+      trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+      cover: 'assets/images/guardios-da-galaxia.jpg',
+      name: 'Guardiões da Galaxia',
+      relevance: 100,
+      age: 18,
+      parts: 5,
+      categories: ['Épico', 'Filme de Fantasia', 'Viagem no espaço']
+    }
+  ];
+
+  setMenuState(state: boolean){
+    this.menuOpened = state;
+  }
+
+  closeMenu(){
+    this.menuOpened = false;
+  }
+
 }
